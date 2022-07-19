3638e8ad4e7543bfd7c907eb34607806b455cbfb
Edgallen <popov567@gmail.com>
2022-07-19 18:00:26 +0300

    добавление проверки типов в feed

--- src/components/OrdersList/OrdersList.tsx
+++ src/components/OrdersList/OrdersList.tsx
@@ -19,38 +19,42 @@ const Images: FC<TImages> = ({order}) => {
 
     const getImage = (id: string): string => {
         const ingredientObj = storeIngredients.filter((ingredient: TItem) => ingredient._id === id);
-        return ingredientObj[0].image
+        return ingredientObj[0].image;
     };
 
     return (
-        <div className={styles.orders__images}>
-            {slicedImages.map((ingredient: string) => (
-                <div className={styles.orders__img__box} key={uuid()}>
-                    <div className={`${styles.orders__img__container} mr-4`}>
-                        <img
-                            src={getImage(ingredient)}
-                            className={`${styles.orders__card__img} mr-4`}
-                            alt="ingredient_image"
-                        />
+        <>
+            {(<div className={styles.orders__images}>
+                {slicedImages.map((ingredient: string) => (
+                    <div className={styles.orders__img__box} key={uuid()}>
+                        <div className={`${styles.orders__img__container} mr-4`}>
+                            {ingredient && (
+                                <img
+                                    src={getImage(ingredient)}
+                                    className={`${styles.orders__card__img} mr-4`}
+                                    alt="ingredient_image"
+                                />
+                            )}
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
 
-            {length > 5 && (
-                <div className={styles.orders__img__box}>
-                    <div className={styles.orders__count__container}>
-                        <p className='text text_type_main-default'>{`+${order.ingredients.slice(5, length).length}`}</p>
+                {length > 5 && (
+                    <div className={styles.orders__img__box}>
+                        <div className={styles.orders__count__container}>
+                            <p className='text text_type_main-default'>{`+${order.ingredients.slice(5, length).length}`}</p>
+                        </div>
+                        <div className={`${styles.orders__img__container} mr-4`}>
+                            {order.ingredients[5] && (<img
+                                src={getImage(order.ingredients[5])}
+                                className={`${styles.orders__card__img} mr-4`}
+                                alt="ingredient_image"
+                            />)}
+                        </div>
                     </div>
-                    <div className={`${styles.orders__img__container} mr-4`}>
-                        <img
-                            src={getImage(order.ingredients[5])}
-                            className={`${styles.orders__card__img} mr-4`}
-                            alt="ingredient_image"
-                        />
-                    </div>
-                </div>
-            )}
-        </div>
+                )}
+            </div>)}
+        </>
     )
 }
 
@@ -68,13 +72,25 @@ const OrdersList: FC<IOrdersList> = ({type, order}) => {
         : navigate(`/profile/orders/${order._id}`, {state: { background: location }});
     };
 
-    const getTotalPrice = (): number => {
+    const getTotalPrice = (): number | string => {
         let total: number = 0;
+        let error: boolean = false;
 
-        order.ingredients.forEach((ingredient: string) => {
-            total += storeIngredients.filter((storeIngredient: TItem) => 
-            storeIngredient._id === ingredient)[0].price;
-        })
+        if (storeIngredients.length > 0 && order.ingredients.length > 0) {
+            order.ingredients.forEach((ingredient: string) => {
+                if (ingredient) {
+                    total += storeIngredients.filter((storeIngredient: TItem) =>
+                    storeIngredient._id === ingredient)[0].price;
+                }
+                if (!ingredient) {
+                    error = true;
+                }
+            });
+        }
+
+        if (error) {
+            return 'Ошибка'
+        }
 
         return total;
     };
@@ -101,34 +117,38 @@ const OrdersList: FC<IOrdersList> = ({type, order}) => {
     }, []);
 
     return (
-        <div className={styles.orders__card} onClick={handleOrderClick}>
-            <div className={`${styles.orders__info}`}>
-                <h3 className="text text_type_digits-default">#{order.number}</h3>
-                <p className="text text_type_main-default text_color_inactive">{formatDate(new Date(order.createdAt))}</p>
-            </div>
-
-            <div className={`${styles.orders__title} ${styles.type}`}>
-                <h2 className={`text text_type_main-medium ${type === 'profile' ? 'mb-2': ''}`}>{order.name}</h2>
-                {type === 'profile' && (
-                    <p 
-                        className={`${status === 'Выполнен' ? styles.orders__text__ready : ''} text text_type_main-default`}
-                    >
-                        {status}
-                    </p>
-                )}
-            </div>
-
-            <div className={styles.orders__list}>
-                <div>
-                    <Images order={order} />
+        <>
+        {storeIngredients.length > 0 && order.ingredients.length > 0 && (
+            <div className={styles.orders__card} onClick={handleOrderClick}>
+                <div className={`${styles.orders__info}`}>
+                    <h3 className="text text_type_digits-default">#{order.number}</h3>
+                    <p className="text text_type_main-default text_color_inactive">{formatDate(new Date(order.createdAt))}</p>
                 </div>
 
-                <div className={styles.orders__price}>
-                    <h2 className="text text_type_digits-default">{getTotalPrice()}</h2>
-                    <CurrencyIcon type="primary"/>
+                <div className={`${styles.orders__title} ${styles.type}`}>
+                    <h2 className={`text text_type_main-medium ${type === 'profile' ? 'mb-2': ''}`}>{order.name}</h2>
+                    {type === 'profile' && (
+                        <p 
+                            className={`${status === 'Выполнен' ? styles.orders__text__ready : ''} text text_type_main-default`}
+                        >
+                            {status}
+                        </p>
+                    )}
+                </div>
+
+                <div className={styles.orders__list}>
+                    <div>
+                        <Images order={order} />
+                    </div>
+
+                    <div className={styles.orders__price}>
+                        <h2 className="text text_type_digits-default">{getTotalPrice()}</h2>
+                        <CurrencyIcon type="primary"/>
+                    </div>
                 </div>
             </div>
-        </div>
+        )}
+        </>
     )
 }
 
--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,32 +1,49 @@
 import React, { useEffect } from 'react';
 import styles from './feed.module.css';
 import FeedDetails from '../components/FeedDetails/FeedDetails';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 import {useAppDispatch, useAppSelector} from "../utils/hooks";
 import { wsFeedClose, wsFeedInit } from '../services/actions/wsActions';
 import { WSUrl } from '../utils/fetchData';
 import { TOrder } from '../types';
 import { selectFeedIngredient } from '../services/actions/modal';
+import { useLocation } from 'react-router';
+import { getCookie } from '../utils/cookies';
 
 export const OrderPage = () => {
   const dispatch = useAppDispatch();
   const params = useParams();
   const feedMessages = useAppSelector((store) => store.webSocket.feedMessages);
   const length = feedMessages.length;
+  const auth = useAppSelector((store) => store.auth.isAuth)
+  const location = useLocation();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    dispatch(wsFeedInit(`${WSUrl}/all`));
+    console.log(auth);
+    console.log(length > 0);
+    if (location.pathname.slice(1, 5) === 'feed') {
+      dispatch(wsFeedInit(`${WSUrl}/all`));
+    }
+
+    if (location.pathname.slice(1, 8) === 'profile') {
+      !auth ? dispatch(wsFeedInit(`${WSUrl}?token=${getCookie('token')}`)) : navigate('/login', { replace: true });
+    }
+
     return () => {
       dispatch(wsFeedClose());
     }
   }, [dispatch]);
 
-  useEffect(() => {
-    if (length > 0 && length < 2) {
-      const orderObj = feedMessages[length - 1].orders.filter((order: TOrder) => order._id === params.id);
-      dispatch(selectFeedIngredient(orderObj[0]))
-    }
-  }, [length])
+  // useEffect(() => {
+  //   if(length) {
+  //     if (length > 0 && length < 2) {
+  //       console.log(feedMessages);
+  //       const orderObj = feedMessages[length - 1].orders.filter((order: TOrder) => order._id === params.id);
+  //       dispatch(selectFeedIngredient(orderObj[0]));
+  //     }
+  //   }
+  // }, [length])
 
   return (
     <section className={styles.order}>
